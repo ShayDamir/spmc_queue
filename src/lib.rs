e3428178b02e6e97880289fe20f56814b74e272e
Damir Shaikhutdinov <lost404@gmail.com>
2021-03-06 21:39:40 +0100

    Fix integer overflows and implement drop.
    Initialize head and tail to usize::MAX to trigger overflow on the first
    operation.
    
    Use wrapping operations to advance head and tail and calculate size.
    
    Add drop() implementation.
    
    Add to_consumer() implementation to allow making a consumer out of
    existing producer.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,20 @@ struct SpmcQueueInner<T> {
     capacity: usize,
 }
 
+impl<T> Drop for SpmcQueueInner<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.load(Acquire);
+        let tail = self.tail.load(Acquire);
+        let contents = self.contents.as_ptr();
+        while head != tail {
+            let idx = head % self.capacity;
+            let result = unsafe { core::ptr::read(contents.add(idx)) };
+            core::mem::drop(result);
+            head = head.wrapping_add(1);
+        }
+    }
+}
+
 pub struct SpmcQueueProducer<T> {
     inner: Arc<SpmcQueueInner<T>>,
 }
@@ -24,9 +38,9 @@ unsafe impl<T: Send> Sync for SpmcQueueConsumer<T> {}
 
 pub fn spmc_new<T>(capacity: usize) -> (SpmcQueueProducer<T>, SpmcQueueConsumer<T>) {
     let inner = SpmcQueueInner {
-        head: AtomicUsize::new(0),
-        tail: AtomicUsize::new(0),
-        capacity: capacity,
+        head: AtomicUsize::new(usize::MAX),
+        tail: AtomicUsize::new(usize::MAX),
+        capacity,
         contents: Vec::with_capacity(capacity),
     };
     assert!(capacity != 0);
@@ -39,7 +53,7 @@ pub fn spmc_new<T>(capacity: usize) -> (SpmcQueueProducer<T>, SpmcQueueConsumer<
 
 impl<T> SpmcQueueProducer<T> {
     fn new(inner: Arc<SpmcQueueInner<T>>) -> Self {
-        Self { inner: inner }
+        Self { inner }
     }
 
     pub fn enqueue(&mut self, item: T) -> bool {
@@ -47,24 +61,25 @@ impl<T> SpmcQueueProducer<T> {
         let head = inner.head.load(Acquire);
         let tail = inner.tail.load(Acquire);
         let capacity = inner.capacity;
-        if tail - head >= capacity {
+        if tail.wrapping_sub(head) >= capacity {
             return false;
         }
         let idx = tail % capacity;
         let contents = inner.contents.as_ptr();
         unsafe {
-            // memcpy(&inner.contents[idx], item, sizeof(item));
-            // ownership is also transferred to the inner.contents
             core::ptr::write((contents as *mut T).add(idx), item);
         }
-        inner.tail.store(tail + 1, Release);
+        inner.tail.store(tail.wrapping_add(1), Release);
         true
+    }
+    pub fn to_consumer(&self) -> SpmcQueueConsumer<T> {
+        SpmcQueueConsumer::new(self.inner.clone())
     }
 }
 
 impl<T> SpmcQueueConsumer<T> {
     fn new(inner: Arc<SpmcQueueInner<T>>) -> Self {
-        Self { inner: inner }
+        Self { inner }
     }
     pub fn dequeue(&self) -> Option<T> {
         let inner = &self.inner;
@@ -81,11 +96,11 @@ impl<T> SpmcQueueConsumer<T> {
             let result = unsafe { core::ptr::read(contents.add(idx)) };
             match inner
                 .head
-                .compare_exchange_weak(head, head + 1, AcqRel, Acquire)
+                .compare_exchange_weak(head, head.wrapping_add(1), AcqRel, Acquire)
             {
                 Ok(_) => return Some(result),
                 Err(x) => {
-                    std::mem::forget(result);
+                    core::mem::forget(result);
                     head = x;
                 }
             }
@@ -94,13 +109,29 @@ impl<T> SpmcQueueConsumer<T> {
     pub fn size(&self) -> usize {
         let head = self.inner.head.load(Acquire);
         let tail = self.inner.tail.load(Acquire);
-        tail - head
+        tail.wrapping_sub(head)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::spmc_new;
+    use core::sync::atomic::AtomicUsize;
+    use core::sync::atomic::Ordering::Relaxed;
+    #[derive(Debug, PartialEq)]
+    struct DropTest(i32);
+    static DROP_TEST_COUNT: AtomicUsize = AtomicUsize::new(0);
+    impl Drop for DropTest {
+        fn drop(&mut self) {
+            DROP_TEST_COUNT.fetch_sub(1, Relaxed);
+        }
+    }
+    impl DropTest {
+        fn new() -> Self {
+            DROP_TEST_COUNT.fetch_add(1, Relaxed);
+            DropTest(0)
+        }
+    }
     #[test]
     fn single_thread_usize() {
         let (mut p, c) = spmc_new(128);
@@ -136,7 +167,25 @@ mod tests {
         assert!(p.enqueue(Box::new(5)));
         assert_eq!(c.dequeue(), Some(Box::new(5)));
         assert_eq!(c.dequeue(), None);
+        let c2 = p.to_consumer();
+        assert!(p.enqueue(Box::new(42)));
+        assert_eq!(c2.dequeue(), Some(Box::new(42)));
+        assert_eq!(c2.dequeue(), None);
     }
+
+    #[test]
+    fn single_thread_drop_test() {
+        let (mut p, c) = spmc_new(128);
+        assert!(p.enqueue(DropTest::new()));
+        assert_eq!(c.dequeue(), Some(DropTest::new()));
+        assert_eq!(c.dequeue(), None);
+        assert!(p.enqueue(DropTest::new()));
+
+        core::mem::drop(p);
+        core::mem::drop(c);
+        assert_eq!(DROP_TEST_COUNT.load(Relaxed), 0);
+    }
+
     #[test]
     fn multi_thread_box() {
         let (mut p, c) = spmc_new(128);
@@ -159,7 +208,7 @@ mod tests {
         for handle in consumers {
             match handle.join() {
                 Ok(_) => continue,
-                Err(x) => panic!(x),
+                Err(x) => panic!("thread join failed {:?}", x),
             }
         }
     }
