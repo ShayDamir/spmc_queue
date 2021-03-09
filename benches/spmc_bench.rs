4f6de75ee2ae183a1018365e1925e5164056dde6
Damir Shaikhutdinov <lost404@gmail.com>
2021-03-09 23:23:34 +0100

    Fix formatting with cargo fmt.

--- benches/spmc_bench.rs
+++ benches/spmc_bench.rs
@@ -2,13 +2,13 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use spmc::spmc_new;
 
 fn single_thread_bench(c: &mut Criterion) {
-	let (mut producer, consumer) = spmc_new(128);
-	c.bench_function("single", |b| {
-		b.iter(|| {
-			producer.enqueue(black_box(20));
-			consumer.dequeue().unwrap();
-		})
-	});
+    let (mut producer, consumer) = spmc_new(128);
+    c.bench_function("single", |b| {
+        b.iter(|| {
+            producer.enqueue(black_box(20));
+            consumer.dequeue().unwrap();
+        })
+    });
 }
 
 criterion_group!(benches, single_thread_bench);
